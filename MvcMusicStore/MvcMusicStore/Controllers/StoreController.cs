ad301e5209fe6b7da6a13b14147633ddca0c729b
BasilDaniel <vasilev.d.s@mail.ru>
2017-11-12 00:28:16 +0300

    store updated

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -4,38 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcMusicStore.Models;
+using System.Data.Entity;
 
 namespace MvcMusicStore.Controllers
 {
     public class StoreController : Controller
     {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
         public ActionResult Index()
         {
-            var genres = new List<Genre>
-            {
-                new Genre { Name = "Disco"},
-                new Genre { Name = "Jazz"},
-                new Genre { Name = "Rock"},
-                new Genre { Name = "House"},
-                new Genre { Name = "Trance"},
-                new Genre { Name = "Jungle"},
-                new Genre { Name = "DubStep"}
-            };
+            var genres = storeDB.Genres.ToList();
+            
             return View(genres);
         }
         //
         // GET: /Store/Browse
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
-
+            var genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
+               
             return View(genreModel);
         }
         //
         // GET: /Store/Details
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
             return View(album);
         }
     }
